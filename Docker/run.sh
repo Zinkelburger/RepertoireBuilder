b97ad0d47baa9e2c2ca060b0a32051d4f1e3c3e4
Zinkelburger <andrewlbernal@gmail.com>
2023-05-26 21:07:32 -0400

    Added a Docker folder to make the files more readable

--- Docker/run.sh
+++ Docker/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Read the database volume location from the configuration file
-databaseVolumeLocation=$(grep "^databaseVolumeLocation=" configuration.txt | cut -d'=' -f2)
+databaseVolumeLocation=$(grep "^databaseVolumeLocation=" ../configuration.txt | cut -d'=' -f2)
 
 if ! docker start my-database; then
     docker run -d --name my-database -p 5432:5432 -v $databaseVolumeLocation:/var/lib/postgresql/data database-image
